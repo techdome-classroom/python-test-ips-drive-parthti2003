64d575c7e8b4c6aeda772d130334c96a4bdaeb68
akash06032 <akashsamaiya111@gmail.com>
2024-04-26 13:42:19 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,5 +1,26 @@
 def longest_substring(s: str) -> int:
+    """
+    Implementation of the longestSubstring function.
+    """
+    longest_substring_length = 0
     
+    for i in range(len(s)):
+        current_string_set = set()
+        
+        for x in range(i, len(s)):
+            if s[x] in current_string_set:
+                break
+            else:
+                current_string_set.add(s[x])
+        
+        longest_substring_length = max(
+            longest_substring_length,
+            len(current_string_set)
+        )
+    
+    return longest_substring_length
+
+
     """"
     
     Implement the function longest_substring 
@@ -8,6 +29,3 @@ def longest_substring(s: str) -> int:
 
     """ 
     pass
-
-
-
